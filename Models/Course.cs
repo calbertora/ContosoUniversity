4fd0a25256ccdd52ab4f7949b322a8c2cc6addb9
Carlos Ramirez <calbertora@gmail.com>
2020-02-18 19:56:02 -0500

    Add the models to the app

---
+++ Models/Course.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
