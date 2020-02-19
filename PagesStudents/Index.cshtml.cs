f070df61589ee9f2991e79b17058415a6a4ce548
Carlos Ramirez <calbertora@gmail.com>
2020-02-18 20:23:48 -0500

    Change the Context to add all the classes

--- PagesStudents/Index.cshtml.cs
+++ PagesStudents/Index.cshtml.cs
@@ -23,7 +23,7 @@ namespace ContosoUniversity.PagesStudents
 
         public async Task OnGetAsync()
         {
-            Student = await _context.Student.ToListAsync();
+            Student = await _context.Students.ToListAsync();
         }
     }
 }
