0a4cb96024221d4aab8fc549f32f9645ffaf5f20
Carlos Ramirez <calbertora@gmail.com>
2020-02-18 21:01:36 -0500

    Add the seed method to seed the db

--- Models/Enrollment.cs
+++ Models/Enrollment.cs
@@ -10,7 +10,7 @@ namespace ContosoUniversity.Models
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
-        public Grade? GradeID { get; set; }
+        public Grade? Grade { get; set; }
 
         public Course Course { get; set; }
         public Student Student { get; set; }
